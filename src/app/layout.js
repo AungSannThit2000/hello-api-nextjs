a9d941483c50c70f316e10978fdd29813cc8dc71
AungSannThit2000 <astshiroe@gmail.com>
2026-02-07 22:54:44 +0700

    fixed bugs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const sans = Inter({ variable: "--font-sans", subsets: ["latin"] });
+const mono = Roboto_Mono({ variable: "--font-mono", subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -19,9 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${sans.variable} ${mono.variable} antialiased`}>
         {children}
       </body>
     </html>
